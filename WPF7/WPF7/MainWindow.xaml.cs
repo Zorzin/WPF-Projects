11591955b4bd4c0d03496fb62863dc3325bf6c4a
Szymon <zorzin0os@gmail.com>
2016-04-15 00:14:22 +0200

    ADD WPF7

---
+++ WPF7/WPF7/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF7.Annotations;
+
+namespace WPF7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> comboList; 
+        public Collection<Osoba> lista = new ObservableCollection<Osoba>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DodajRegion();
+            ComboBox.ItemsSource = comboList;
+            ListBox.ItemsSource = lista;
+        }
+
+        private void DodajButton_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Add(new Osoba());
+            ListBox.SelectedIndex = ListBox.Items.Count - 1;
+            imieText.Focus();
+        }
+
+        private void UsunButton_Click(object sender, RoutedEventArgs e)
+        {
+            lista.RemoveAt(ListBox.SelectedIndex);
+
+        }
+
+        private void DodajRegion()
+        {
+            comboList = new List<string>();
+            comboList.Add("Białystok");
+            comboList.Add("Warszawa");
+            comboList.Add("Poznań");
+            comboList.Add("Kraków");
+            comboList.Add("Wrocław");
+        }
+        
+    }
+}
---
+++ WPF7/WPF7/Osoba.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF7
+{
+    public class Osoba : INotifyPropertyChanged
+    {
+        public bool szczegoly { get; set; }
+        private string imie;
+        private string nazwisko;
+        private string email;
+        public string Imie
+        {
+            get { return imie; }
+            set
+            { imie = value; OnPropertyChanged("wyswietl"); }
+        }
+
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set
+            { nazwisko = value; OnPropertyChanged("wyswietl"); }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            { email = value; OnPropertyChanged("wyswietl"); }
+        }
+        public int Kwota { get; set; }
+        public string Region { get; set; }
+        public int Dostep { get; set; }
+
+        public string wyswietl
+        {
+            get { return imie + " " + nazwisko + " (" + email + ")"; }
+        }
+
+        public Osoba()
+        {
+            this.Imie = null;
+            this.Nazwisko = null;
+            this.Email = null;
+            this.Kwota = 0;
+            this.Region = null;
+            this.Dostep = 0;
+            this.szczegoly = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this,
+                new PropertyChangedEventArgs(property));
+        }
+    }
+}
